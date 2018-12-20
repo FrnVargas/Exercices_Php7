313ab28409f9cef0e2c4c27c08bbfdddcb557d09
FV13 <43671750+fv13@users.noreply.github.com>
2018-12-20 19:40:04 +0100

    Update aritmetica.php

--- aritmetica.php
+++ aritmetica.php
@@ -1,9 +1,7 @@
 <?php
 require ('../Ejercicios_php_7/funcion.php');
 //recoger datos
-
 $num1 =funcion:: datoIntroducido('num1');
 $num2 =funcion:: datoIntroducido('num2');
-
 $res = funcion:: aritmetica($num1,$num2);
 print_r ($res);
