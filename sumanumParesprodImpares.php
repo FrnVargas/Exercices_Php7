7887f61d68a2a13258e2188457d0c626daf5c195
FV13 <43671750+fv13@users.noreply.github.com>
2019-02-06 20:02:38 +0100

    Update sumanumParesprodImpares.php

--- sumanumParesprodImpares.php
+++ sumanumParesprodImpares.php
@@ -1,10 +1,7 @@
 <?php
-
 require ('../Ejercicios_php_7/funcion.php');
 //recoger Datos
-
 $max = funcion::datoIntroducido('max');
-
 $res= funcion::sumaPares_ProductoImpares($max);
 $salida= 'Resultado: '.$res;
 print_r($salida);
